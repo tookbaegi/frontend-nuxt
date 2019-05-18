71bd2f1e5f1b7b43d53a76ccf5688a9eb0642d77
도다 <admin@ddark.kr>
2019-05-18 22:57:00 +0900

    components: 헤더 프로그레스 컴포넌트화

---
+++ components/HeadProgress.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="title-bar">
+        <div class="title-bar-progress" v-bind:style="{ width: progressPer }"></div>
+    </div>
+</template>
+
+<script>
+export default {
+    props: ['per'],
+    data: function() {
+        return {
+            progressPer: this.per
+        }
+    }
+}
+</script>
--- pages/home.vue
+++ pages/home.vue
@@ -1,14 +1,18 @@
 <template>
   <div class="home">
     <div class="title">
-        <div class="title-bar"></div>
         <h1>그 사람은 누구인가요?</h1>
     </div>
   </div>
 </template>
 
 <script>
+import HeadProgress from '~/components/HeadProgress.vue'
+
 export default {
+    components: {
+        HeadProgress
+    },
     layout: 'futures'
 }
 </script>
@@ -17,9 +21,16 @@ export default {
 .home {
     .title {
         .title-bar {
-            
+            width: 90px;
+            height: 3px;
+            background: rgb(177, 177, 177);
+            .title-bar-progress {
+                height: 100%;
+                background: #000;
+            }
         }
         h1 {
+            margin-top: 19px;
             color: rgb(138, 137, 137);
             font-weight: 600;
             font-size: 30px;
