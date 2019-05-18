fc7400ffd8a0ec52a6392b79c85aa60e6d05a0e8
도다 <admin@ddark.kr>
2019-05-19 01:48:08 +0900

    view: "퀘스트 완료" 100%! 아아

--- pages/quest/final.vue
+++ pages/quest/final.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="home">
+  <div class="final">
     <div class="title">
         <HeadProgress per=90 />
         <h1>퀘스트명 입력</h1>
@@ -15,6 +15,19 @@
                 완료하기
             </a>
         </div>
+        <div class="title">
+            <h1>다른 유저의<br>완료된 퀘스트</h1>
+        </div>
+        <section class="btns">
+            <a href="#" class="btn" v-for="(data, i) in listData" :key="i">
+                {{ data.string }}
+            </a>
+        </section>
+        <div class="com-btn-box">
+            <a href="#" class="btn">
+                더 보기
+            </a>
+        </div>
     </div>
   </div>
 </template>
@@ -23,6 +36,27 @@
 import HeadProgress from '~/components/HeadProgress.vue'
 
 export default {
+    asyncData() {
+        const listData = [
+            {
+                'name': 'snsn',
+                'string': '회식자리에서 부장님 모르게 신속히 9시에 빠져나가기'
+            },
+            {
+                'name': 'snsn',
+                'string': '대중교통 투머치토커 인내하기'
+            },
+            {
+                'name': 'snsn',
+                'string': '썸남썸녀'
+            },
+            {
+                'name': 'snsn',
+                'string': '아 졸려'
+            }
+        ]
+        return { listData }
+    },
     components: {
         HeadProgress
     },
@@ -31,7 +65,7 @@ export default {
 </script>
 
 <style lang="scss">
-.home {
+.final {
     .title {
         h1 {
             margin-top: 19px;
@@ -68,16 +102,50 @@ export default {
             justify-items: stretch;
             align-items: stretch;
             .ta-btn {
-                background: rgb(229, 229, 229);
+                background: rgba(229, 229, 229, 0.37);
+                border-radius: 5px;
                 text-align: center;
                 font-size: 20px;
                 color: rgb(128, 128, 128);
                 padding: 19px 0;
                 letter-spacing: -1px;
+                margin: 5px;
             }
             margin-bottom: 80px;
         }
         
+    }
+    .btns {
+        width: 100%;
+        margin-bottom: 20px;
+        display: grid;
+        grid-template-columns: 50% 50%;
+        justify-items: stretch;
+        align-items: stretch;
+        .btn {
+            font-size: 15pt;
+            font-weight: 400;
+            display: flex;
+            color: rgb(107, 105, 105);
+            letter-spacing: -.9px;
+            background: rgb(229, 229, 229);
+            height: 170px;
+            margin: 5px;
+            padding: 20px;
+            border-radius: 5px;
+        }
+        .btn-other {
+            background: rgb(243, 237, 237);
+        }
+    }
+    .com-btn-box {
+        .btn {
+            display: block;
+            background: rgba(229,229,229, 0.4);
+            width: 100%;
+            text-align: center;
+            padding: 20px 0;
+        }
     }
 }
 </style>
