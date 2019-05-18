a1e4549f95894c9105fb2ba7be983fe1eda9d606
skyepodium <skyepodium@gmail.com>
2019-05-18 21:58:08 +0900

    index.vue에 스플래쉬 추가

--- pages/index.vue
+++ pages/index.vue
@@ -1,17 +1,33 @@
 <template>
-
+  <div class="title">
+    뚝배기대신퀘스트
+  </div>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
-
 export default {
-  components: {
-    Logo
+  created () {
+    this.splash()
+  },
+  methods: {
+    splash() {
+    setTimeout(() => { 
+      this.$router.push({'name': 'login'})
+      }, 3000);    
+
+    }
   }
 }
 </script>
 
 <style>
-
+  .title {
+    width: calc(100% - 80px);
+    margin-left:auto;
+    margin-right:auto;
+    text-align:center;
+    margin-top:280px;
+    font-size:30px;
+    font-weight: 500;
+  }
 </style>
