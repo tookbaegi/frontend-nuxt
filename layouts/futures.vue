59fd89db94142b565d6779f7f990a37547324c62
도다 <admin@ddark.kr>
2019-05-18 22:19:47 +0900

    layout: 내부 기능 레이아웃 추가

---
+++ layouts/futures.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+      <nav>
+          <a href="#" class="menu-icon"></a>
+      </nav>
+    <nuxt />
+  </div>
+</template>
+
+<style lang="scss">
+*,
+*:before,
+*:after {
+  box-sizing: border-box;
+  margin: 0;
+}
+body {
+  font-family: 'Spoqa Han Sans', sans-serif;
+}
+nav {
+    margin-top: 56px;
+    margin-bottom: 24px;
+    height: 27.5px;
+}
+</style>
